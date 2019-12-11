73800e48d5b828bd0b1eb427db021ea09e6e6df0
calvezer <ervin.calvez19@gmail.com>
2019-12-11 20:34:19 +0100

    java & googleCalendar
    Communication avec l'agenda Google en Java

--- GoogleCalendar/Code Java/src/main/java/CalendarQuickstart.java
+++ GoogleCalendar/Code Java/src/main/java/CalendarQuickstart.java
@@ -1,20 +1,3 @@
-# **Utilisation de Google Calendar**
-
-Google Calendar est un outil qui permet de tenir son emploi du temps jour après jour.
-
-## Pourquoi l'utilisons-nous ?
-
-L'objectif d'utiliser Google Calendar est dans un premier temps de pratiquer l'utilisation d'un API. En effet, Google propose de nombreux API pour nombreuses de leurs applications. C'est le cas pour Google Calendar.
-
-## Qu'allons-nous faire ?
-
-Dans un premier temps, nous voulons afficher les éléments qui sont présents dans notre agenda.
-
-## Qu'avons nous mis en place pour réussir cela ?
-
-D'abord, il faut récupérer et importer les différentes bibliothèques et librairies nécessaires.
-
-```java
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -39,19 +22,25 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
-```
+public class CalendarQuickstart {
+    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-Une fois ceci fait, on peut s'atteler à communiquer avec notre emploi du temps !!!
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-On va essayer d'afficher dans la console de notre IDE, les prochains cours qui sont programmés !
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
 
-Pour ce faire, nous devons créer une application et obtenir un id et clé d'authentification qui sera utilisé dans le programme principal.
-
-Ces accès sont stockés dans le fichier creditentials.json.
-
-Le code afin de vérifier si les accès existent est :
-
-```java
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -69,13 +58,7 @@ Le code afin de vérifier si les accès existent est :
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
-```
 
-Ensuite, nous allons devoir récupérer nos données et nos cours exportés depuis Aurion.
-
-Pour cela, on va appeler le Calendrier :
-
-```java
     public static void main(String... args) throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -106,15 +89,4 @@ Pour cela, on va appeler le Calendrier :
             }
         }
     }
-```
-
-On affiche le paramètre "primary" car c'est lui qui contient normalement le titre du cours.
-
-Cependant, pour certaines données du calendrier, le programme plante car le summary est vide et ne contient aucune donnée.
-
-Dans le cas où les 10 prochains événements sont correctement remplis au niveau du summary, on obtient l'affichage suivant :
-
-![](C:\Users\Ervin\Documents\GitHub\ANDAPP\GoogleCalendar\Images\Results_Java.PNG)
-
-Voilà, maintenant il serait intéressant de repartir de ces données et de les ranger dans un json par exemple. Avec ce format, on pense qu'il serait possible de les exploiter et les utiliser pour faire une interface sous Android Studio.
-
+}
